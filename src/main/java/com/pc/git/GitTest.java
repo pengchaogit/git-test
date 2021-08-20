acadf47ca8cce38195157c595fc84707640c7e52
pengchao <1428199962@qq.com>
2021-08-20 10:27:20 +0800

    first commit

---
+++ src/main/java/com/pc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.pc.git;
+
+/**
+ * @author pc
+ * @create 2021-08-20-10:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
