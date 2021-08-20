fa08d44b8de3011351179b13a9a39053562cada6
pengchao <1428199962@qq.com>
2021-08-20 11:10:15 +0800

    second commit

---
+++ src/main/java/com/pc/git/GitTest02.java
@@ -0,0 +1,11 @@
+package com.pc.git;
+
+/**
+ * @author pc
+ * @create 2021-08-20-11:09
+ */
+public class GitTest02 {
+    public static void main(String[] args) {
+        System.out.println("GitTest02.class");
+    }
+}
